3f49a74d822b57cf290eb123683c5a322f992795
JudeLingan <judecohenlingan@gmail.com>
2025-10-25 02:04:12 -0400

    theme script added

---
+++ theme.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+THEME_DIR=$HOME/.config/themes
+THEME=$(ls $THEME_DIR | rofi -dmenu)
+
+#return if rofi is closed
+if [ -z $THEME ]; then
+	exit 0
+fi
+
+programs=(
+	"rofi"
+	"alacritty"
+	"waybar"
+	"river"
+)
+for i in "${programs[@]}"; do
+	ln -f $THEME_DIR/$THEME/colors-$i.* $HOME/.config/$i/
+done
+
+ln -f $THEME_DIR/$THEME/colors-nvim.lua $HOME/.config/nvim/lua/config
+
+pkill waybar
+riverctl spawn waybar
+
+source $HOME/.config/river/colors-river.sh
+riverctl background-color $color_accent
+riverctl border-color-focused $color_fg
+riverctl border-color-unfocused $color_subtle
