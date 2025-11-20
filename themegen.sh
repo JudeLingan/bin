df3d0e70a261c0cc8920ee27c1ae18ff6ba96b21
Jude Lingan <judecohenlingan@gmail.com>
2025-11-20 07:51:55 -0500

    even more it-worksier

--- themegen.sh
+++ themegen.sh
@@ -5,20 +5,22 @@ if [ "$#" -ne 5 ]; then
 	exit 1
 fi
 
+newline=$'\n'
+
 ## waybar
 waybarcolors="
-@define-color bg #$1;\n
-@define-color fg #$2;\n
-@define-color subtle #$3;\n
-@define-color accent #$4;\n
+@define-color bg #\$1;$newline
+@define-color fg #$2;$newline
+@define-color subtle #$3;$newline
+@define-color accent #$4;$newline
 @define-color error #$5;"
 
 rivercolors="
 #!/usr/bin/env bash
-color_bg=\"0x$1\"\n
-color_fg=\"0x$2\"\n
-color_subtle=\"0x$3\"\n
-color_accent=\"0x$4\"\n
+color_bg=\"0x$1\"$newline
+color_fg=\"0x$2\"$newline
+color_subtle=\"0x$3\"$newline
+color_accent=\"0x$4\"$newline
 color_error=\"0x$5\""
 
 echo $waybarcolors > colors-waybar.css
