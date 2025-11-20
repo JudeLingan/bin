f0e9e9a9f20f264b919e8a975159a59215186fc4
Jude Lingan <judecohenlingan@gmail.com>
2025-11-20 08:10:40 -0500

    switched to printf:

--- themegen.sh
+++ themegen.sh
@@ -5,23 +5,7 @@ if [ "$#" -ne 5 ]; then
 	exit 1
 fi
 
-newline=$'\n'
-
 ## waybar
-waybarcolors="
-@define-color bg #\$1;$newline
-@define-color fg #$2;$newline
-@define-color subtle #$3;$newline
-@define-color accent #$4;$newline
-@define-color error #$5;"
+printf '@define-color bg #%q;\n@define-color fg #%q;\n@define-color subtle #%q;\n@define-color accent #%q;\n@define-color error #%q;' $1 $2 $3 $4 $5 > colors-waybar.css
 
-rivercolors="
-#!/usr/bin/env bash
-color_bg=\"0x$1\"$newline
-color_fg=\"0x$2\"$newline
-color_subtle=\"0x$3\"$newline
-color_accent=\"0x$4\"$newline
-color_error=\"0x$5\""
-
-echo $waybarcolors > colors-waybar.css
-echo $rivercolors > colors-river.sh
+printf '#!/usr/bin/env bash\ncolor_bg="0x$1"\ncolor_fg="0x$2"\ncolor_subtle="0x$3"\ncolor_accent="0x$4"\ncolor_error="0x$5"' $1 $2 $3 $4 $5 > colors-river.sh
