62041bcbd9e0c18ee5ad4dc623f4a25e297e6bcd
JudeLingan <judecohenlingan@gmail.com>
2025-11-30 15:06:17 -0500

    fixed themegen.sh river colors

--- themegen.sh
+++ themegen.sh
@@ -8,4 +8,4 @@ fi
 ## waybar
 printf '@define-color bg #%q;\n@define-color fg #%q;\n@define-color subtle #%q;\n@define-color accent #%q;\n@define-color error #%q;' $1 $2 $3 $4 $5 > colors-waybar.css
 
-printf '#!/usr/bin/env bash\ncolor_bg="0x$1"\ncolor_fg="0x$2"\ncolor_subtle="0x$3"\ncolor_accent="0x$4"\ncolor_error="0x$5"' $1 $2 $3 $4 $5 > colors-river.sh
+printf '#!/usr/bin/env bash\ncolor_bg="0x%q"\ncolor_fg="0x%q"\ncolor_subtle="0x%q"\ncolor_accent="0x%q"\ncolor_error="0x%q"' %q %q %q %q %q > colors-river.sh
